e53f3e4bc034c80db79a8d6c939a5e87f91bf01c
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2013-04-27 01:36:30 -0400

    Commit for backup to server

---
+++ js/enter_course.js
@@ -0,0 +1,105 @@
+//  enter_course.js
+
+$(function()
+{
+  //  Get complete list of discipline codes and names to use as selection items.
+  var disciplines   = null;
+  var prompts       = null;
+  var prompt_list   = '';
+  var select_list   = [];
+  var select_index  = 0;
+
+  //  build_prompt_list()
+  //  -----------------------------------------------------------------------------------
+  var build_prompt_list = function(next_char)
+  {
+    var current_value = $('#course').val();
+    if (next_char) current_value += String.fromCharCode(next_char);
+
+    //  TODO; build the regexp from the value, not from all the disciplines??
+
+    $('#prompt-list').empty();
+    for (var i = 0; i < disciplines.length; i++)
+    {
+      var this_regex = disciplines[i].regex;
+      if (this_regex.test(current_value))
+      {
+        var startTag = '<li>';
+        if ( i == select_index ) startTag = "<li class='highlight'>";
+        $(startTag + disciplines[i].prompt + '</li>').appendTo('#prompt-list');
+      }
+      else
+      {
+        console.log(current_value + ' does not match ' + this_regex);
+      }
+    }
+  }
+
+  //  Get disciplines from db
+  //  -----------------------------------------------------------------------------------
+  $.ajax({url:'get_disciplines.php'}).done(function(data)
+  {
+    disciplines = $.parseJSON(data);
+    for (var i = 0; i < disciplines.length; i++)
+    {
+      var code = disciplines[i].code;
+      var name = disciplines[i].name;
+      disciplines[i].prompt = code + ' - ' + name;
+      var regex = '^\s*' + code + '[\w\s]*';
+      for (var n = 0; n < name.length; n++)
+      {
+        regex += name[n] + '[\w\s]*';
+      }
+      disciplines[i].regex = new RegExp(regex, 'i');
+    }
+    $("<ul id='prompt-list'></ul>").appendTo('form');
+    build_prompt_list(0);
+  });
+
+
+  /*    up/down arrows: Highlight next/previous list item.
+   *    tab/enter:      If an element is highlighted, use its value. Otherwise
+   *                    just dismiss the prompt list without
+   *                    changing the value entered.
+   *    other:          Use input value to filter
+   */
+    //  Keypress gives you ASCII chars, including enter
+    $('#course').keypress(function(event)
+    {
+      if ( event.which > 31 && event.which < 123 )
+      {
+        build_prompt_list(event.which);
+      }
+    });
+
+    //  Keyup gives you bs, with repeats
+    $('#course').keyup(function(event)
+    {
+      if (event.which == 8) build_prompt_list(0);
+    });
+
+    //  Keydown gives you up, dn, with repeats
+    $('#course').keydown(function(event)
+    {
+      switch (event.which)
+      {
+        //  Totally rebuild the select list, with proper element highlighted.
+        //  TODO: this is a horribly inefficient way to handle arrows.
+        case 38:  //  up arrow
+                  if (select_index > 0)
+                  {
+                    select_index--;
+                    build_prompt_list(0);
+                  }
+                  break;
+        case 40:  //  down arrow
+                  if ( select_index < (disciplines.length -1) )
+                  {
+                    select_index++;
+                    build_prompt_list(0);
+                  }
+                  break;
+        default:  break;
+      }
+    });
+});
---
+++ tests/enter_course.php
@@ -0,0 +1,83 @@
+<?php
+//  /tests/enter_course.php
+
+set_include_path(get_include_path()
+    . PATH_SEPARATOR . getcwd() . '/../scripts'
+    . PATH_SEPARATOR . getcwd() . '/../include');
+require_once('init_session.php');
+
+//  Test enter_course widget
+//  -------------------------------------------------------------------------------------
+  $mime_type = "text/html";
+  $html_attributes="lang=\"en\"";
+  if ( array_key_exists("HTTP_ACCEPT", $_SERVER) &&
+        (stristr($_SERVER["HTTP_ACCEPT"], "application/xhtml") ||
+         stristr($_SERVER["HTTP_ACCEPT"], "application/xml") )
+       ||
+       (array_key_exists("HTTP_USER_AGENT", $_SERVER) &&
+        stristr($_SERVER["HTTP_USER_AGENT"], "W3C_Validator"))
+     )
+  {
+    $mime_type = "application/xhtml+xml";
+    $html_attributes = "xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"en\"";
+    header("Content-type: $mime_type");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+  }
+  else
+  {
+    header("Content-type: $mime_type; charset=utf-8");
+  }
+?>
+<!DOCTYPE html>
+<html <?php echo $html_attributes;?>>
+  <head>
+    <title>Enter Course Test</title>
+    <script type="text/javascript" src="../js/jquery.min.js"></script>
+    <script type="text/javascript" src="../js/enter_course.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/curriculum.css" />
+    <style type='text/css'>
+      #prompt-list {
+        position: absolute;
+        top:0;
+        right: 0;
+        background-color:white;
+        list-style-type: none;
+        padding:0;
+      }
+      #prompt-list li { padding: 0.25em; margin:0;
+      border: 1px solid black;
+      }
+      form {
+        position: relative;
+      }
+      .highlight {
+        background-color:#99f;
+        color:white;
+       }
+    </style>
+  </head>
+  <body>
+
+  <h1>Enter Course Test</h1>
+<?php
+  //  Show course(s) selected if form was submitted.
+  if ( !empty($form_name) and $form_name === 'enter_course_test')
+  {
+    $course = sanitize($_POST['course']);
+    echo "<h2>'$course'</h2>\n";
+  }
+?>
+    <form action='enter_course.php' method='post'>
+      <input  type='hidden'  
+              name='form-name' 
+              value='enter_course_test' />
+      <fieldset>
+        <label for='course'>Course</label>
+        <input  type='text'    
+                name='course' 
+                id='course'
+                autocomplete='off' />
+      </fieldset>
+    </form>
+  </body>
+</html>
