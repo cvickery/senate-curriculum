83c5f57638805f62de07cc87b4a6c553194fd973
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2013-12-12 22:47:40 -0500

    Switch to PHPMailer.
    Re-code mail_setup.php; expand testing in test_mail_setup.php;
    move sanitize() to its own file so it can be used without having to
    access the database. (test_mail_setup needed this last feature, but
    it has been an ongoing issue.)

--- scripts/mail_setup.php
+++ scripts/mail_setup.php
@@ -1,9 +1,6 @@
 <?php
 
-require_once('Mail.php');
-require_once('Mail/mime.php');
-require_once('Mail/mail.php');
-require_once('credentials.inc');
+require_once('PHPMailerAutoload.php');
 
 //  class Mail
 //  -----------------------------------------------------------------------------------
@@ -11,77 +8,52 @@ require_once('credentials.inc');
  */
 class Senate_Mail
 {
-  /*  Default headers; may be replaced
-   *    From, Reply_to, Date, Message-ID
-   */
-    private $headers = null;
-  /*  Required headers for sending; no default values
-   *    To, Subject
-   */
-    private $recipients = '';
-  /*
-   *  Optional headers, values will be appended to To if necessary
-   *    Cc, Bcc
-   */
+  //  The PHPMailer object.
+  private $mail;
+
   //  Message bodies
     private $text_body = null, $html_body = null;
-    private $mime, $smtp_mail;
+    private $error_message = '';
 
-    private $message = '';
   //  Constructor
   //  --------------------------------------------------------------------------------------
   /*  From, To, Subject, and text message are required.
    *  Be sure to include "real name" in the From header.
    */
-  function __construct($from, $to, $subject, $text_body, $html_body = null, $headers = null)
+  function __construct($from_str, $to_str, $subject, $text_body, $html_body = null)
   {
-    global $smtp_user, $smtp_pass;
-    $this->headers = array(
-        'From'        =>  $from,
-        'To'          =>  $to,
-        'Subject'     =>  $subject,
-        'Reply-to'    =>  $from,
-        'Date'        =>  date('r'),
-        'Message-ID'  =>  '<' . uniqid() . '@senate.qc.cuny.edu>'
-        );
-    $this->recipients = $to;
-    $this->subject = $subject;
-    $this->text_body = $text_body;
-    if ($html_body) $this->html_body = $html_body;
+    //  Create the PHPMailer object and set the encoding
+    $this->mail = new PHPMailer();
+    $this->mail->CharSet = 'UTF-8';
+
+    //  Extract name and address from from_str and to_str
+    $from_array = $this->parse_address($from_str, 'Academic Senate');
+    $this->mail->setFrom($from_array[0], $from_array[1]);
+    $to_array = $this->parse_address($to_str, '');
+    $this->mail->addAddress($to_array[0], $to_array[1]);
+    $this->mail->Subject = $subject;
+    $this->mail->AltBody = $text_body;
+    if ($html_body)
+    {
+      $this->mail->msgHTML($html_body);
+    }
     else
     {
-      $this->html_body = str_replace("\n", "<br/>", $text_body);
+      $this->mail->msgHTML(str_replace("\n", "<br/>", $text_body));
     }
-    if ($headers)
-    {
-      foreach ($headers as $name => $value)
-      {
-        $this->headers[$name] = $value;
-        if (strtolower($name) === 'cc' || strtolower($name === 'bcc'))
-        {
-          $this->add_recipient($value);
-        }
-      }
-    }
-    //  Initialize the Mail_mime obect
-    $crlf = "\n";
-    $this->mime = new Mail_mime(
-        array
-        (
-          'eol' => $crlf,
-          'text_charset' => 'utf-8',
-          'html_charset' => 'utf-8'
-        ));
+  }
 
-    //  Set up the smtp Mail object
-    $smtpinfo["host"] = "smtp.qc.cuny.edu";
-    $smtpinfo["port"] = "25";
-    $smtpinfo["auth"] = true;
-    $smtpinfo["username"] = $smtp_user;
-    $smtpinfo["password"] = $smtp_pass;
-    $smtpinfo["localhost"] = 'senate.qc.cuny.edu';
-    $smtpinfo["persist"] = false;
-    $this->smtp_mail =& Mail::factory('smtp', $smtpinfo);
+  //  Utilities
+  private function parse_address($address, $name = '')
+  {
+    //  Return an array: element 0 is address; 1 is name
+    $returnVal = array($address, $name);
+    if (preg_match('/(.*)\s*<(.*)>/', $address, $matches))
+    {
+      $returnVal[0] = $matches[2];
+      $returnVal[1] = $matches[1];
+    }
+    return $returnVal;
   }
 
   //  Setters
@@ -89,59 +61,34 @@ class Senate_Mail
   {
     //  Setting text generates html, but that can be overridden by
     //  setting html afterwards.
-    $this->text_body = $text_body;
-    $this->html_body = str_replace("\n", "<br/>", $text_body);
+    $this->mail->AltBody = $text_body;
+    $this->mail->msgHTML(str_replace("\n", "<br/>", $text_body));
   }
-  function set_html($html_body)   { $this->html_body = $html_body; }
-  function set_subject($subject)  { $this->headers['Subject'] = $subject; }
+  function set_html($html_body)   { $this->mail->msgHTML($html_body); }
+  function set_subject($subject)  { $this->mail->Subject = $subject; }
   //  add_recipient()
   function add_recipient($recipient)
   {
-    //  Do not duplicate
-    if (! strstr($this->recipients, $recipient))
-    {
-      $this->recipients .= ",$recipient";
-    }
+    $to_array = $this->parse_address($recipient);
+    $this->mail->addAddress($to_array[0], $to_array[1]);
   }
   //  add_cc()
   function add_cc($cc)
   {
-    if (isset($this->headers['Cc']))
-    {
-      //  Do not duplicate
-      if (! strstr($this->headers['Cc'], $cc))
-      {
-        $this->headers['Cc'] .= ",$cc";
-      }
-    }
-    else
-    {
-      $this->headers['Cc'] = $cc;
-    }
-    $this->add_recipient($cc);
+    $cc_array = $this->parse_address($cc);
+    $this->mail->addCC($cc_array[0], $cc_array[1]);
   }
   //  add_bcc()
   function add_bcc($bcc)
   {
-    if (isset($this->headers['Bcc']))
-    {
-      //  Do not duplicate
-      if (! strstr($this->headers['Bcc'], $bcc))
-      {
-        $this->headers['Bcc'] .= ",$bcc";
-      }
-    }
-    else
-    {
-      $this->headers['Bcc'] = $bcc;
-    }
-    $this->add_recipient($bcc);
+    $bcc_array = $this->parse_address($bcc);
+    $this->mail->addBCC($bcc_array[0], $bcc_array[1]);
   }
 
   //  get_message()
   //  --------------------------------------------------------------
-  function getMessage()  { return $this->message; }
-  function get_message() { return $this->message; }
+  function getMessage()  { return $this->error_message; }
+  function get_message() { return $this->error_message; }
 
   //  send()
   //  --------------------------------------------------------------
@@ -150,25 +97,12 @@ class Senate_Mail
    */
   function send()
   {
-    //  Validate headers
-    //  NOTE From    required in constructor
-    //  NOTE To      required in constructor
-    //  NOTE Subject required in constructor
-    //  NOTE Text body required in constructor; html generated
-    //       from it automatically.
-    //  NOTE Recipient list generated automatically
-
-    //  Prep and send
-    $this->mime->setTXTBody($this->text_body);
-    $this->mime->setHTMLBody($this->html_body);
-    $body = $this->mime->get();
-    $hdrs = $this->mime->headers($this->headers);
-    $mime_status = $this->smtp_mail->send($this->recipients, $hdrs, $body);
-    if ( PEAR::isError($mime_status) )
+    if (! $this->mail->send() )
     {
-      $this->message = $mime_status->getMessage();
+      $this->error_message = $this->mail->ErrorInfo;
       return false;
     }
+    $this->error_message = '';
     return true;
   }
 }
--- scripts/test_mail_setup.php
+++ scripts/test_mail_setup.php
@@ -1,12 +1,33 @@
-#! /usr/bin/php
-
 <?php
 require('mail_setup.php');
-  $mail = new Senate_Mail('Testing Email<do-not-reply@qc.cuny.edu>', 
-      'Christopher.Vickery@qc.cuny.edu', 'testing',
-      "This is a nice message.");
+require('sanitize.php');
+$html_msg = <<<EOD
+
+<h2>This is a "swell" message, don't you think?</h2>
+<h2>This is a “smart” message, don’t you think?</h2>
+
+EOD;
+$text_msg = <<<EOD
+
+  This is a "swell" message, don't you think?
+  This is a “smart” message, don’t you think?
+
+EOD;
+  $mail = new Senate_Mail('Christopher Vickery<Christopher.Vickery@qc.cuny.edu>',
+      'Christopher.Vickery@qc.cuny.edu', "'testing' don’t question authority",
+      sanitize($text_msg), sanitize($html_msg));
   $mail->add_cc('vickery@babbage.cs.qc.cuny.edu');
+  $mail->add_bcc('cvickery@gmail.com', 'Charles Christopher Vickery');
+  echo "<pre>\n"; var_dump($mail); echo "</pre>\n";
   $status = $mail->send();
-  if (! $status) echo $mail->getMessage() . "\n";
-  else var_dump($mail);
+  if (! $status)
+  {
+    echo "<h1>Failed</h1>\n" . $mail->getMessage() . "\n<pre>";
+    var_dump($mail);
+    echo "</pre>\n";
+  }
+  else
+  {
+    echo "<h1>Mail Sent!</h1>\n";
+  }
  ?>
