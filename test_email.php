31df1ccb8efb734fb7eb554d785d02d2ec12068b
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2015-09-16 15:24:20 -0400

    initial commit

---
+++ test_email.php
@@ -0,0 +1,28 @@
+<?php
+require_once('scripts/mail_setup.php');
+$submitter_email = 'Christopher.Vickery@qc.cuny.edu';
+$webmaster_email = 'yourself';
+$email_sender     = 'An Academic Senate Robot';
+
+  //  text-only version
+  $text_msg = <<<EOD
+This is your text test message
+
+EOD;
+
+  //  HTML version
+  $html_msg = <<<EOD
+
+<p>This is your HTML test message</p>
+
+EOD;
+  $mail = new Senate_Mail('QC Curriculum<nobody@qc.cuny.edu>', $submitter_email,
+    "This is a test message",
+     $text_msg, $html_msg);
+  $mail->send() or die( $mail->getMessage() .
+      " <a href='.'>try again</a> or report the problem to $webmaster_email");
+
+  echo "<h1>No error to report</h1>";
+  exit;
+
+?>
