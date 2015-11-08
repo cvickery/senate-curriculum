1bfd78fdc097209494024ed49bc313e4de7dc251
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2015-11-08 10:18:17 -0500

    Add timestamp to html and text messages

--- test_email.php
+++ test_email.php
@@ -1,23 +1,22 @@
 <?php
 require_once('scripts/mail_setup.php');
-$recipient_email = 'Christopher.Vickery@qc.cuny.edu';
-$webmaster_email = 'yourself';
+$recipient_email  = 'Christopher.Vickery@qc.cuny.edu';
 $email_sender     = 'An Academic Senate Robot';
-
+$timestamp        = date("l F j, Y H:i");
   //  text-only version
   $text_msg = <<<EOD
-This is your text test message
+This is your text test message, sent: $timestamp
 
 EOD;
 
   //  HTML version
   $html_msg = <<<EOD
 
-<p>This is your HTML test message</p>
+<p>This is your HTML test message, sent: $timestamp</p>
 
 EOD;
   $mail = new Senate_Mail('QC Curriculum<nobody@qc.cuny.edu>', $recipient_email,
-    "This is a test message",
+    "Jack’s Alive",
      $text_msg, $html_msg);
   $mail->add_recipient('cvickery@gmail.com');
   $mail->send() or die( $mail->getMessage() .
