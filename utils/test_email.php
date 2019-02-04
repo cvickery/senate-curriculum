30f55ac89b5d23d72bc54ae74608150c0abdf77b
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-02-03 22:58:38 -0500

    test_email.php migrates to mail.py

--- utils/test_email.php
+++ utils/test_email.php
@@ -3,7 +3,8 @@ require_once('../scripts/mail_setup.php');
 $recipient_email  = 'Christopher.Vickery@qc.cuny.edu';
 $email_sender     = 'An Academic Senate Robot';
 $timestamp        = date("l F j, Y H:i");
-  //  text-only version
+
+  //  plain text version
   $text_msg = <<<EOD
 This is your text test message, sent: $timestamp
 
@@ -15,14 +16,32 @@ EOD;
 <p>This is your HTML test message, sent: $timestamp</p>
 
 EOD;
-  $mail = new Senate_Mail('QC Curriculum<nobody@qc.cuny.edu>', $recipient_email,
-    "Jack’s Alive",
-     $text_msg, $html_msg);
-  $mail->add_recipient('cvickery@gmail.com');
-  $mail->send() or die( $mail->getMessage() .
-      " <a href='.'>try again</a> or report the problem to $webmaster_email");
 
+$plain_name = tempnam(, 'plain');
+$plain_file = fopen($plain_name, 'w');
+$plain_file.write($text_msg);
+$html_name = tempnam(, 'html');
+$html_file = fopen($html_name, 'w');
+$html_file.write($text_msg);
+
+system("/Users/vickery/bin/mail.py -s 'Jack’s Alive' -t $plain_file -h $html_file" .
+       "-f 'An Academic Senate Robot' -t $recipient_email cvickery@gmail.com", $return_value);
+if ($return_value === 0)
+{
   echo "<h1>Test message appears to have been sent. No errors reported.</h1>";
-  exit;
+}
+else
+{
+  echo "<h1>*** test_email failed</h1>";
+}
+exit;
+  // $mail = new Senate_Mail('QC Curriculum<nobody@qc.cuny.edu>', $recipient_email,
+  //   "Jack’s Alive",
+  //    $text_msg, $html_msg);
+  // $mail->add_recipient('cvickery@gmail.com');
+  // $mail->send() or die( $mail->getMessage() .
+  //     " <a href='.'>try again</a> or report the problem to $webmaster_email");
+
+  // exit;
 
 ?>
