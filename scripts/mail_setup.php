9ba9266387f4377396400139c66ce85d884db88a
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-02-06 23:58:01 -0500

    Update mail_setup
    Replace PHP mailer with mail.py
    Ready for testing.

--- scripts/mail_setup.php
+++ scripts/mail_setup.php
@@ -1,19 +1,12 @@
 <?php
 
-require_once('../vendor/phpmailer/phpmailer/PHPMailerAutoload.php');
 
-//  class Mail
+//  class Senate_Mail
 //  -----------------------------------------------------------------------------------
-/*    Try to simplify the interface to sending MIME mail using smpt to mail.qc.cuny.edu
+/*    Interface to /Users/vickery/bin/mail.py
  */
 class Senate_Mail
 {
-  //  The PHPMailer object.
-  private $mail;
-
-  //  Message bodies
-    private $text_body = null, $html_body = null;
-    private $error_message = '';
 
   //  Constructor
   //  --------------------------------------------------------------------------------------
@@ -22,67 +15,69 @@ class Senate_Mail
    */
   function __construct($from_str, $to_str, $subject, $text_body, $html_body = null)
   {
-    //  Create the PHPMailer object and set the encoding
-    $this->mail = new PHPMailer();
-    $this->mail->CharSet = 'UTF-8';
-
-    //  Extract name and address from from_str and to_str
-    $from_array = $this->parse_address($from_str, 'Academic Senate');
-    $this->mail->setFrom($from_array[0], $from_array[1]);
-    $to_array = $this->parse_address($to_str, '');
-    $this->mail->addAddress($to_array[0], $to_array[1]);
-    $this->mail->Subject = $subject;
-    $this->mail->AltBody = $text_body;
+    //  Save constructor params
+    $this->error_message = 'Message created; no send operation attempted yet.';
+    $this->from_addr = $from_str;
+    $this->to_addrs = array($to_str);
+    $this->subject = $subject;
+    $this->cc_addrs = array();
+    $this->bcc_addrs = array();
+    $this->plain_name = tempnam('/tmp/', 'plain');
+    $plain_file = fopen($this->plain_name, 'w');
+    fwrite($plain_file, $text_body);
+    fclose($plain_file);
+    chmod($this->$plain_name, 0644);
+    $this->html_name = NULL;
     if ($html_body)
     {
-      $this->mail->msgHTML($html_body);
+      $this->$html_name = tempnam('/tmp/', 'html');
+      $html_file = fopen($this->html_name, 'w');
+      fwrite($html_file, $html_msg);
+      fclose($html_file);
+      chmod($this->html_name, 0644);
     }
-    else
-    {
-      $this->mail->msgHTML(str_replace("\n", "<br/>", $text_body));
-    }
-  }
 
-  //  Utilities
-  private function parse_address($address, $name = '')
-  {
-    //  Return an array: element 0 is address; 1 is name
-    $returnVal = array($address, $name);
-    if (preg_match('/(.*)\s*<(.*)>/', $address, $matches))
-    {
-      $returnVal[0] = $matches[2];
-      $returnVal[1] = $matches[1];
-    }
-    return $returnVal;
-  }
+  //   //  Extract name and address from from_str and to_str
+  //   $from_array = $this->parse_address($from_str, 'Academic Senate');
+  //   $this->mail->setFrom($from_array[0], $from_array[1]);
+  //   $to_array = $this->parse_address($to_str, '');
+  //   $this->mail->addAddress($to_array[0], $to_array[1]);
+  //   $this->mail->Subject = $subject;
+  //   $this->mail->AltBody = $text_body;
+  //   if ($html_body)
+  //   {
+  //     $this->mail->msgHTML($html_body);
+  //   }
+  //   else
+  //   {
+  //     $this->mail->msgHTML(str_replace("\n", "<br/>", $text_body));
+  //   }
+  // }
 
   //  Setters
-  function set_text($text_body)
-  {
-    //  Setting text generates html, but that can be overridden by
-    //  setting html afterwards.
-    $this->mail->AltBody = $text_body;
-    $this->mail->msgHTML(str_replace("\n", "<br/>", $text_body));
-  }
-  function set_html($html_body)   { $this->mail->msgHTML($html_body); }
-  function set_subject($subject)  { $this->mail->Subject = $subject; }
+  // function set_text($text_body)
+  // {
+  //   //  Setting text generates html, but that can be overridden by
+  //   //  setting html afterwards.
+  //   $this->mail->AltBody = $text_body;
+  //   $this->mail->msgHTML(str_replace("\n", "<br/>", $text_body));
+  // }
+  // function set_html($html_body)   { $this->mail->msgHTML($html_body); }
+  // function set_subject($subject)  { $this->mail->Subject = $subject; }
   //  add_recipient()
   function add_recipient($recipient)
   {
-    $to_array = $this->parse_address($recipient);
-    $this->mail->addAddress($to_array[0], $to_array[1]);
+    $this->to_addrs[] = $recipient;
   }
   //  add_cc()
-  function add_cc($cc)
+  function add_cc($recipient)
   {
-    $cc_array = $this->parse_address($cc);
-    $this->mail->addCC($cc_array[0], $cc_array[1]);
+    $this->cc_addrs[] = $recipient;
   }
   //  add_bcc()
-  function add_bcc($bcc)
+  function add_bcc($recipient)
   {
-    $bcc_array = $this->parse_address($bcc);
-    $this->mail->addBCC($bcc_array[0], $bcc_array[1]);
+    $this->bcc_addrs[] = $recipient;
   }
 
   //  get_message()
@@ -92,18 +87,61 @@ class Senate_Mail
 
   //  send()
   //  --------------------------------------------------------------
-  /*  Validate headers and send the email. Return true on success,
+  /*  Create header args and send the email. Return true on success,
    *  false on failure. Use getMessage to find out what went wrong.
    */
   function send()
   {
-    if (! $this->mail->send() )
+    $cmd = "SMTP_SERVER=smtp.qc.cuny.edu /Users/vickery/bin/mail.py";
+    $cmd .= " -f $this->from_addr";
+    $cmd .= " -s '$this->subject'";
+    $cmd .= " -p '$this->plain_name'";
+    if (! is_null($this->html_name))
     {
-      $this->error_message = $this->mail->ErrorInfo;
+      $cmd .= " -h $this->html_name";
+    }
+    if (count($this->cc_addr) > 0)
+    {
+      $cc_list = implode(', ', $this->cc_addrs);
+      $cmd .= " -c $cc_list";
+    }
+    if (count($this->bcc_addr) > 0)
+    {
+      $bcc_list = implode(', ', $this->bcc_addrs);
+      $cmd .= " -b $bcc_list";
+    }
+    $recipients = implode(', ', $this->to_addrs);
+    $cmd .= " -- $recipients";
+
+    $msg_file = tempnam('/tmp/', 'msg');
+    system("$cmd 2> $msg_file", $exit_status);
+
+    unlink($this->plain_name);
+    if (! is_null($this->html_name))
+    {
+      unlink($this->html_name);
+    }
+
+    if ($exit_status != 0)
+    {
+      $this->error_message = get_file_contents($msg_file);
+      unlink($msg_file);
       return false;
     }
-    $this->error_message = '';
-    return true;
+    else
+    {
+      $this->error_message = 'Message sent.';
+      unlink($msg_file);
+      return true;
+    }
+
+    // if (! $this->mail->send() )
+    // {
+    //   $this->error_message = $this->mail->ErrorInfo;
+    //   return false;
+    // }
+    // $this->error_message = '';
+    // return true;
   }
 }
 ?>
