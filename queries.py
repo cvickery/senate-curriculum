4cd4ac47125b2919305bb9e3c4c09b3b07f2cfa5
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2019-06-14 15:50:06 -0400

    Update queries.py
    Links to additional queries besides the ones used for transfer app.

--- queries.py
+++ queries.py
@@ -13,9 +13,19 @@ print("""
   <head>
     <title>CUNYfirst Course Information</title>
     <style>
+      body {
+      padding: 1em;
+      font-family: sans-serif;
+      }
       li {
       white-space:pre;
       font-family: monospace;
+      }
+      a {
+      text-decoration: none;
+      }
+      a:hover {
+      font-size: 1.1em;
       }
     </style>
   </head>
@@ -31,6 +41,15 @@ for query in latest_queries.glob('*.csv'):
   while bytes > 1000:
     bytes /= 1000
     power += 1
-  print(f'<li>{bytes:>5.1f}{suffixes[power]}B <a href="{query}" download>{query.name}</a></li>')
-
+  print(f'<li>{bytes:>5.1f} {suffixes[power]}B <a href="{query}" download>{query.name}</a></li>')
+other_queries = Path('./other_queries')
+update_date = datetime.fromtimestamp(other_queries.stat().st_mtime).strftime('%B %e, %Y')
+print(f'</ul><h1>Other Queries</h1>\n<p>Last updated {update_date}</p><ul>')
+for query in other_queries.glob('*.csv'):
+  bytes = query.stat().st_size
+  power = 0
+  while bytes > 1000:
+    bytes /= 1000
+    power += 1
+  print(f'<li>{bytes:>5.1f} {suffixes[power]}B <a href="{query}" download>{query.name}</a></li>')
 print('</ul></body></html>')
